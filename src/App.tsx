d0ee7ff63cbdfae458cc0fd37e762c6e646eebb0
Maycon Douglas <mdrs.maycon@gmail.com>
2023-10-20 17:02:40 -0300

    update: All

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { PrivateRoutes } from './pages/privateRoutes';
+import { LoginPage } from './pages/login/LoginPage';
+import { CadastroPage } from './pages/cadastro/CadastroPage';
+import { HomePage } from './pages/home/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<PrivateRoutes/>}>
+          <Route path='/home' element={<HomePage />}></Route>
+          <Route path="*" element={<Navigate to="/home" />} />
+      </Route>
+      <Route path='/cadastro' element={<CadastroPage />}></Route>
+      <Route path='/login' element={<LoginPage />}></Route>
+    </Routes>
   );
 }
 
